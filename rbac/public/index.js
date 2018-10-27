a958ae37b5a35a82c3603d3f2ffa3c4cdd20e6a3
Vineeth Gutta <vineethgutta@gmail.com>
2018-10-27 23:07:04 +0000

    init server.js for routing

---
+++ rbac/public/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#google").click(function(){console.log("Google")});
+    
+    $("#facebook").click(function(){console.log("Facebook")});
+    
+    $("#twitter").click(function(){console.log("Twitter")});
+});
